52bbec746b80f3c739f101b28ad9c76c274b4561
Kevin Gifford <ksgifford@gmail.com>
2017-08-11 15:36:28 -0700

    Adding finalresults.py

---
+++ finalresults.py
@@ -0,0 +1,17 @@
+import sys
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
+
+if len(sys.argv) < 1:
+    cur.execute("SELECT * FROM tweetwordcount ORDER BY word")
+    records = cur.fetchall()
+    for rec in records:
+        print rec[0], " ", rec[1], '\n'
+elif len(sys.argv) = 1:
+    word = sys.argv[0]
+    cur.execute("SELECT count FROM tweetwordcount WHERE word=%s", (word,))
+    wrd_count = cur.fetchall()
+    print 'Total number of occurences of "', word, '": ', wrd_count[0][0]
