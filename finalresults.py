5a593c35906f6ab1675df13d5463014efe4634ad
Kevin Gifford <ksgifford@gmail.com>
2017-08-11 15:43:36 -0700

    Edits to finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -5,13 +5,13 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
     cur.execute("SELECT * FROM tweetwordcount ORDER BY word")
     records = cur.fetchall()
     for rec in records:
         print rec[0], " ", rec[1], '\n'
-elif len(sys.argv) == 1:
-    word = sys.argv[0]
+elif len(sys.argv) == 2:
+    word = sys.argv[1]
     cur.execute("SELECT count FROM tweetwordcount WHERE word=%s", (word,))
     wrd_count = cur.fetchall()
     print 'Total number of occurences of "', word, '": ', wrd_count[0][0]
