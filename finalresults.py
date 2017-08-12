cfe043b956ef9f7435712dd3811ba3bc96b9b592
Kevin Gifford <ksgifford@gmail.com>
2017-08-11 18:30:14 -0700

    Fixing error handling in finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -14,9 +14,9 @@ elif len(sys.argv) == 2:
     word = sys.argv[1]
     cur.execute("SELECT count FROM tweetwordcount WHERE word=%s", (word,))
     wrd_count = cur.fetchall()
-    try:
-        print 'Total number of occurences of "', word, '": ', wrd_count[0][0]
-    except IndexError:
+    if not wrd_count:
         print "I'm sorry, that word wasn't found."
+    else:
+        print 'Total number of occurences of "', word, '": ', wrd_count[0][0]
 else:
     print "Too many arguments! Please enter a single word."
