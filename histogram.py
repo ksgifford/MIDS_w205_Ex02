f8968ee8ff6e4d252fd60cb27e36c74e930bfab8
Kevin Gifford <ksgifford@gmail.com>
2017-08-11 18:01:42 -0700

    Refactored histogram to accept variant inputs.

--- histogram.py
+++ histogram.py
@@ -5,14 +5,21 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 
-if len(sys.argv) < 3:
-    print "Not enough arguments. Please specify an upper and lower bound."
-elif len(sys.argv) == 3:
-    low = sys.argv[1].split(',')[0]
-    high = sys.argv[2].split(',')[0]
+def wrd_fetch(low,high):
     cur.execute("SELECT * FROM tweetwordcount WHERE count >= %s AND count <= %s ORDER BY count DESC", (low,high))
     wrd_list = cur.fetchall()
     for wrd in wrd_list:
         print wrd[0], ':', wrd[1]
-else:
+
+if len(sys.argv) == 2:
+    low = sys.argv[1].split(',')[0]
+    high = sys.argv[1].split(',')[1]
+    wrd_fetch(low,high)
+elif len(sys.argv) == 3:
+    low = sys.argv[1].split(',')[0]
+    high = sys.argv[2].split(',')[0]
+    wrd_fetch(low,high)
+elif len(sys.argv) > 3:
     print "Too many arguments! Please enter an upper and lower bound."
+else:
+    print "Not enough arguments. Please specify an upper and lower bound."
