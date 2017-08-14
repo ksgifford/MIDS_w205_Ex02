0f4800a29ff8aef9580a7727d98fea18449bd2f5
Kevin Gifford <ksgifford@gmail.com>
2017-08-13 18:35:17 -0700

    Adding Readme.txt and credentials_template.py.

---
+++ credentials_template.py
@@ -0,0 +1,11 @@
+def credentials():
+    tweet_cred = {
+        "consumer_key"        :  "your info here",
+        "consumer_secret"     :  "your info here",
+        "access_token"        :  "your info here",
+        "access_token_secret" :  "your info here"
+    }
+
+    return tweet_cred
+
+credentials()
